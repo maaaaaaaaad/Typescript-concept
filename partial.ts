7ca41ce1bcf74dfc6d388495d7cb891da199107c
오동웅 <woong@MacPro.local>
2021-04-22 23:34:33 +0900

    partial.ts

---
+++ partial.ts
@@ -0,0 +1,34 @@
+{
+  type ToDo = {
+    title: string;
+    subTitle?: string;
+    description: string;
+    feeling: string;
+    prioritry: "high" | "low";
+  };
+
+  function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
+    return { ...todo, ...fieldsToUpdate };
+  }
+
+  const todo: ToDo = {
+    title: "Typescript",
+    subTitle: "Type",
+    description: "Learn Typescript",
+    feeling: "Lovely",
+    prioritry: "high",
+  };
+
+  const fieldsToUpdate = {
+    title: "nono",
+  };
+
+  const updated = updateTodo(todo, fieldsToUpdate);
+
+  // const updated = updateTodo(todo, {
+  //   subTitle: "WoongScript",
+  //   prioritry: "low",
+  // });
+
+  console.log(updated);
+}
